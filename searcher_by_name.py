8fd3280f4859832c2263764e7f40ac1e37c11f38
Dima de Taller <beleyhha@gmail.com>
2026-05-19 11:23:04 +0200

    refactor: move another part of logic to class, praparing to popeline refactor

--- searcher_by_name.py
+++ searcher_by_name.py
@@ -3,4 +3,13 @@ class SearcherByName:
     def __init__(self, user_input, optional=None, year="0000"):
         self.user_input = user_input
         self.year = year
-        splited_user_input = self.user_input.lower().split()
+        self.split_user_input = self.user_input.split(" ")
+        
+        if optional != None:
+            self.optional = optional.split(" ")
+        else
+            self.optional = []
+            
+        #temp variables for generating
+        self.mutate_variants_of_name = []
+        
